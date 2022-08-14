d31cdb0298a008a82c99cd565c46cf32d6995da1
Felipe Oliveira <fpo@felipeweb.dev>
2022-08-14 16:31:57 -0300

    implement credentials per grpc request based on instance metadata

---
+++ auth.go
@@ -0,0 +1,30 @@
+package cloudrunauth
+
+import (
+	"context"
+	"fmt"
+
+	"cloud.google.com/go/compute/metadata"
+)
+
+type MetadataServerToken struct {
+	serviceURL string
+}
+
+// GetRequestMetadata is called on every request, so we are sure that token is always not expired
+func (t MetadataServerToken) GetRequestMetadata(ctx context.Context, in ...string) (map[string]string, error) {
+	// based on https://cloud.google.com/run/docs/authenticating/service-to-service#go
+	tokenURL := fmt.Sprintf("/instance/service-accounts/default/identity?audience=%s", t.serviceURL)
+	idToken, err := metadata.Get(tokenURL)
+	if err != nil {
+		return nil, fmt.Errorf("cannot query id token for gRPC: %w", err)
+	}
+
+	return map[string]string{
+		"authorization": "Bearer " + idToken,
+	}, nil
+}
+
+func (MetadataServerToken) RequireTransportSecurity() bool {
+	return true
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/funccloud/cloud-run-auth
+
+go 1.19
+
+require cloud.google.com/go/compute v1.8.0
