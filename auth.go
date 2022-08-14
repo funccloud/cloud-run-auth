88e2d0aa385a49bb2576aa9e147f19f1e2719320
Felipe Oliveira <fpo@felipeweb.dev>
2022-08-14 16:46:18 -0300

    expose service url

--- auth.go
+++ auth.go
@@ -8,13 +8,13 @@ import (
 )
 
 type MetadataServerToken struct {
-	serviceURL string
+	ServiceURL string
 }
 
 // GetRequestMetadata is called on every request, so we are sure that token is always not expired
 func (t MetadataServerToken) GetRequestMetadata(ctx context.Context, in ...string) (map[string]string, error) {
 	// based on https://cloud.google.com/run/docs/authenticating/service-to-service#go
-	tokenURL := fmt.Sprintf("/instance/service-accounts/default/identity?audience=%s", t.serviceURL)
+	tokenURL := fmt.Sprintf("/instance/service-accounts/default/identity?audience=%s", t.ServiceURL)
 	idToken, err := metadata.Get(tokenURL)
 	if err != nil {
 		return nil, fmt.Errorf("cannot query id token for gRPC: %w", err)
